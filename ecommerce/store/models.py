64181cbf22935c8450a4abfd883ecd79a9202949
Ainca <a0925119261@gmail.com>
2023-03-05 03:51:43 +0800

    to set media url & image_url, use property decorator dynamic add images

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -16,9 +16,20 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default= False, null=True, blank=False)
-    #image
+    image = models.ImageField(null=True, blank=True)
     def __str__(self):
         return(self.name)
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ""
+            return url
+    ###獲取圖片的URL，如果出現異常則返回空字符串。在這裡，self代表當前的Product對象，self.image代表這個對象的圖片屬性。self.image.url返回這個圖片在媒體文件夾中的URL。如果獲取URL過程中出現異常，則返回一個空字符串。
+
+###使用@property裝飾器將這個方法轉換為屬性之後，我們可以直接通過product.imageURL的方式獲取圖片的URL，而不需要再調用一個方法。這樣做的好處是可以讓代碼更加簡潔，而且使用起來更加方便。因此，@property裝飾器在Django中被廣泛使用，特別是在模型中經常被使用。
     
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
