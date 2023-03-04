4f1be254d05164b5d262f7860e4f5b54dfb100f3
Ainca <a0925119261@gmail.com>
2023-03-05 06:46:19 +0800

    build cart&checkout page

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -17,8 +17,9 @@ class Product(models.Model):
     price = models.FloatField()
     digital = models.BooleanField(default= False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
+
     def __str__(self):
-        return(self.name)
+        return self.name
     
     @property
     def imageURL(self):
@@ -26,8 +27,8 @@ class Product(models.Model):
             url = self.image.url
         except:
             url = ""
-            return url
-    ###獲取圖片的URL，如果出現異常則返回空字符串。在這裡，self代表當前的Product對象，self.image代表這個對象的圖片屬性。self.image.url返回這個圖片在媒體文件夾中的URL。如果獲取URL過程中出現異常，則返回一個空字符串。
+        return url
+###獲取圖片的URL，如果出現異常則返回空字符串。在這裡，self代表當前的Product對象，self.image代表這個對象的圖片屬性。self.image.url返回這個圖片在媒體文件夾中的URL。如果獲取URL過程中出現異常，則返回一個空字符串。
 
 ###使用@property裝飾器將這個方法轉換為屬性之後，我們可以直接通過product.imageURL的方式獲取圖片的URL，而不需要再調用一個方法。這樣做的好處是可以讓代碼更加簡潔，而且使用起來更加方便。因此，@property裝飾器在Django中被廣泛使用，特別是在模型中經常被使用。
     
@@ -40,11 +41,29 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
     
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])   
+        return total 
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+    
+    
 class OrderItem(models.Model):
-    Product = models.ForeignKey(Product, on_delete=models.SET_NULL,blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL,blank=True, null=True)
     order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True)
-    date_added = models.DateTimeField(auto_now_add=True,)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
